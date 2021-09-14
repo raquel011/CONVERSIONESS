a2d5ed914f2a332bc9cbe9b5521bcee9077ead7f
raquel011 <esther_x19@outlook.com>
2021-09-14 10:31:51 -0500

    modificado2

---
+++ kilosgramos.php
@@ -0,0 +1,11 @@
+<?php 
+    
+$operacion=$_POST['lista'];
+switch ($operacion) {
+    case kilogramos:
+       echo $_POST['c1']*1000;
+        break;
+    
+   
+}
+?>
---
+++ mostrar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="convertirKilos.php" method="post" name="p">
+        <label>ingrese un numero (kg)<input type="text" name="c1"/><br/>
+        
+        
+            <label>selecciona para convertir <br/><select name="lista">
+                <option value="kilogramos">kilogramos<br/></option><br/>
+                <br/><br/>
+                <br/>
+               
+                </select> 
+            </label>
+            <input type="submit" value="ver resultado"/><br/>
+    </form>
+</body>
+</html>
